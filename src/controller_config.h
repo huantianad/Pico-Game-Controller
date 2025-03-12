477043ebf95a5d6ca67f2b1fa3749fbf287d2762
huantian <davidtianli@gmail.com>
2025-03-11 18:52:11 -0700

    custom board iidx config

--- src/controller_config.h
+++ src/controller_config.h
@@ -4,14 +4,14 @@
 #define SW_GPIO_SIZE 12               // Number of switches
 #define LED_GPIO_SIZE 12              // Number of switch LEDs
 #define ENC_GPIO_SIZE 1               // Number of encoders
-#define ENC_PPR 24                    // Encoder PPR
-#define MOUSE_SENS 5                  // Mouse sensitivity multiplier
-#define ENC_DEBOUNCE true            // Encoder Debouncing
-#define SW_DEBOUNCE_TIME_US 4000      // Switch debounce delay in us
+#define ENC_PPR 360                   // Encoder PPR
+#define MOUSE_SENS 1                  // Mouse sensitivity multiplier
+#define ENC_DEBOUNCE false            // Encoder Debouncing
+#define SW_DEBOUNCE_TIME_US 6000      // Switch debounce delay in us
 #define ENC_PULSE (ENC_PPR * 4)       // 4 pulses per PPR
 #define REACTIVE_TIMEOUT_MAX 1000000  // HID to reactive timeout in us
-#define WS2812B_LED_SIZE 10           // Number of WS2812B LEDs
-#define WS2812B_LED_ZONES 2           // Number of WS2812B LED Zones
+#define WS2812B_LED_SIZE 20           // Number of WS2812B LEDs
+#define WS2812B_LED_ZONES 1           // Number of WS2812B LED Zones
 #define WS2812B_LEDS_PER_ZONE \
   WS2812B_LED_SIZE / WS2812B_LED_ZONES  // Number of LEDs per zone
 
@@ -22,14 +22,14 @@ const uint8_t SW_KEYCODE[] = {HID_KEY_D, HID_KEY_F, HID_KEY_J, HID_KEY_K,
                               HID_KEY_C, HID_KEY_M, HID_KEY_A, HID_KEY_B,
                               HID_KEY_1, HID_KEY_E, HID_KEY_G};
 const uint8_t SW_GPIO[] = {
-    2, 4, 6, 8, 10, 12, 14, 27, 16, 18, 20, 22,
+    18, 16, 14, 12, 10, 8, 6, 28, 4, 2, 22, 24
 };
 const uint8_t LED_GPIO[] = {
-    3, 5, 7, 9, 11, 13, 15, 25, 17, 19, 21, 26,
+    19, 17, 15, 13, 11, 9, 7, 29, 5, 3, 23, 25,
 };
-const uint8_t ENC_GPIO[] = {0};  // TT(0, 1)
-const bool ENC_REV[] = {false};  // Reverse Encoders
-const uint8_t WS2812B_GPIO = 28;
+const uint8_t ENC_GPIO[] = {26};  // TT(26, 27)
+const bool ENC_REV[] = {true};  // Reverse Encoders
+const uint8_t WS2812B_GPIO = 1;
 
 #endif
 
